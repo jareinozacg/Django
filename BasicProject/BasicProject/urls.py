455bf7f5285bb10e0fd0526311570ee3b5a023ca
Alfonso Reinoza <jareinozacg@gmail.com>
2018-07-24 19:16:09 -0400

    Using templates

--- BasicProject/BasicProject/urls.py
+++ BasicProject/BasicProject/urls.py
@@ -6,6 +6,7 @@ Definition of urls for BasicProject.
 from django.conf.urls import include, url
 import HelloDjangoApp.views
 
+
 # Django processes URL patterns in the order they appear in the array
 
 
