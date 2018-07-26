f9064cbc4e62b012ba5745e9e7c16d3684b712f8
Alfonso Reinoza <jareinozacg@gmail.com>
2018-07-26 17:57:11 -0400

    Models management

---
+++ DjangoWeb/env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\Alfonso\source\repos\LearningDjango\DjangoWeb\env\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
